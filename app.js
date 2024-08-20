48c33c207b13c1b2887cbaf52d36c60a12cbf120
zahirul <zahirulopel@gmail.com>
2024-08-20 12:51:32 +0600

    add middleware for request log and get 404 on unknown endpoint

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const noticeRouter = require("./controllers/notice");
+const middleware = require("./utils/middleware");
 
 app.use(express.json());
 
+app.use(middleware.requestLogger);
 app.use("/api/notice", noticeRouter);
+
+app.use(middleware.unknownEndpoint);
 
 module.exports = app;
