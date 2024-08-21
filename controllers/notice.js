6356b1a0e9262580a8c9697279b3d7c099d4b0e1
zahirul <zahirulopel@gmail.com>
2024-08-21 16:26:49 +0600

    database: create database table users,notices,comments

--- controllers/notice.js
+++ controllers/notice.js
@@ -1,9 +1,27 @@
 const noticeRouter = require("express").Router();
+const logger = require("../utils/logger");
+const db = require("../models/noticeModel");
 
-noticeRouter.get("/", (req, res) => {});
+noticeRouter.get("/all", (req, res) => {
+  db.serialize(() => {
+    const getNotices = `SELECT * FROM "notices";`;
+
+    db.all(getNotices, [], (err, row) => {
+      if (err) {
+        res.status(404).json({ error: "Error on getting notices" });
+      }
+      logger.info("row", row);
+      res.status(200).json(row);
+    });
+  });
+});
 noticeRouter.get("/:noticeId", (req, res) => {});
 noticeRouter.post("/new", (req, res) => {});
 noticeRouter.put("/:noticeId", (req, res) => {});
 noticeRouter.delete("/:noticeId", (req, res) => {});
+
+// db.close((err) => {
+//   err ? logger.info(err.message) : logger.error("Closed database connection");
+// });
 
 module.exports = noticeRouter;
---
+++ models/noticeModel.js
@@ -0,0 +1,65 @@
+const sqlite3 = require("sqlite3").verbose();
+const logger = require("../utils/logger");
+const db = new sqlite3.Database(
+  "./models/noticebord.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) {
+      logger.error("Error while connecting to database", err.message);
+    }
+    logger.info("Connected to database.");
+  }
+);
+
+db.serialize(() => {
+  let createUsersTable = `CREATE TABLE IF NOT EXISTS "users"(
+        "id" INTEGER,
+        "first_name" TEXT NOT NULL,
+        "last_name" TEXT NOT NULL,
+        PRIMARY KEY ("id")
+    );`;
+
+  db.run(createUsersTable, (err) => {
+    err
+      ? logger.error("Error on creating users table", err.message)
+      : logger.info("User table created successfully");
+  });
+
+  let createNoticeTable = `CREATE TABLE IF NOT EXISTS "notices"(
+        "id" INTEGER,
+        "user_id" INTEGER NOT NULL,
+        "title" TEXT NOT NULL,
+        "description" TEXT,
+        "date" DEFAULT CURRENT_DATE,
+        PRIMARY KEY ("id"),
+        FOREIGN KEY ("user_id") REFERENCES "users"("id")
+    );`;
+
+  db.run(createNoticeTable, (err) => {
+    err
+      ? logger.error("Error on creating notices table", err.message)
+      : logger.info("Notices table created successfully");
+  });
+
+  let createCommentsTable = `CREATE TABLE IF NOT EXISTS "comments"(
+        "id" INTEGER,
+        "user_id" INTEGER NOT NULL,
+        "notice_id" INTEGER NOT NULL,
+        "date" DEFAULT CURRENT_DATE,
+        "description" TEXT,
+        PRIMARY KEY ("id"),
+        FOREIGN KEY ("user_id") REFERENCES "users"("id"),
+        FOREIGN KEY ("notice_id") REFERENCES "notices"("id")
+    );`;
+
+  db.run(createCommentsTable, (err) => {
+    err
+      ? logger.error("Error on creating comments table", err.message)
+      : logger.info("Comments table created successfully");
+  });
+  const query = `INSERT INTO "notices"("user_id","title","description")
+        VALUES(?,?,?);
+  `;
+});
+
+module.exports = db;
