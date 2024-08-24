6559fd98da6d11095dd67554dbbb68d5532adece
zahirul <zahirulopel@gmail.com>
2024-08-24 14:54:38 +0600

    feature: new notice will add on notice post request

--- controllers/notice.js
+++ controllers/notice.js
@@ -8,7 +8,7 @@ noticeRouter.get("/all", (req, res) => {
 
     db.all(getNotices, [], (err, row) => {
       if (err) {
-        res.status(404).end();
+        res.status(404).json({ error: "notice not found" }).end();
       }
       res.status(200).json(row);
     });
@@ -19,12 +19,59 @@ noticeRouter.get("/:noticeId", (req, res) => {
   const { noticeId } = req.params;
   db.get(query, [Number(noticeId)], (err, row) => {
     if (err) {
-      res.status(404).end();
+      res
+        .status(403)
+        .json({ error: `Database error on getting ${noticeId}` })
+        .end();
     }
-    res.status(200).json(row);
+    row
+      ? res.status(200).json(row)
+      : res
+          .status(404)
+          .json({ error: `notice id ${noticeId} not found` })
+          .end();
   });
 });
-noticeRouter.post("/new", (req, res) => {});
+noticeRouter.post("/new", (req, res) => {
+  const { user_id, title, description } = req.body;
+
+  db.serialize(() => {
+    const insertTable = `INSERT INTO "notices" ("user_id","title","description")
+    VALUES(?,?,?)
+  `;
+
+    db.run(
+      insertTable,
+      [Number(user_id), String(title), String(description)],
+      (err) => {
+        if (err) {
+          console.log(err);
+          res
+            .status(403)
+            .json({ error: `Database error adding new notice ${err.message}` })
+            .end();
+        }
+      }
+    );
+
+    const getTable = `SELECT * FROM "notices" WHERE "title" = ? AND "description"=?`;
+    db.all(getTable, [String(title), String(description)], (err, row) => {
+      if (err) {
+        res
+          .status(403)
+          .json({ error: `Database error on getting data ${res.message}` })
+          .end();
+      }
+      row
+        ? res.status(200).json(row)
+        : res
+            .status(404)
+            .json({ error: `Notice ${noticeId} not found` })
+            .end();
+    });
+  });
+});
+
 noticeRouter.put("/:noticeId", (req, res) => {});
 noticeRouter.delete("/:noticeId", (req, res) => {});
 
